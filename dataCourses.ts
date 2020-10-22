dbc5912eddaa269705c441a3a1fd26b27818f097
ee-ortiz <ee.ortiz@uniandes.edu.co>
2020-10-21 20:32:18 -0500

    Commit taller

---
+++ dataCourses.ts
@@ -0,0 +1,10 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en equipo", "Pablo Picasso", 3),
+  new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+  new Course("Diseño y análisis de algoritmos", "Jorge Duitama", 3),
+  new Course("Cáculo vectorial", "Camilo Sanabria", 3),
+  new Course("Fisica II", "Luis Quiroga", 3)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW en equipo", "Pablo Picasso", 3),
+    new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+    new Course("Diseño y análisis de algoritmos", "Jorge Duitama", 3),
+    new Course("Cáculo vectorial", "Camilo Sanabria", 3),
+    new Course("Fisica II", "Luis Quiroga", 3)
+];
